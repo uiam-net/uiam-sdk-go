2aad88f2594b59ce93b859ab3a78fe556f4761d8
Zhiming Jiang <lanvige@gmail.com>
2021-02-18 11:28:13 +0800

    update id req

--- models/request/identity_request.go
+++ models/request/identity_request.go
@@ -59,33 +59,32 @@ type IdentityUpdateRequest struct {
 
 // =================== Auth =================== //
 
-// AuthMixinCreateRequest AuthMixinCreateRequest
-type AuthMixinCreateRequest struct {
+// AuthIDCreateRequest AuthIDCreateRequest
+type AuthIDCreateRequest struct {
 	BaseRequest
 
-	IdentityName string               `json:"username"`
-	PhoneCode    string               `json:"phone_code" binding:"required,min=1"`
-	PhoneNumber  string               `json:"phone_number" binding:"required,min=4"`
-	Email        string               `json:"email"`
-	AvatarURL    string               `json:"avatar_url"`
-	Description  string               `json:"description"`
-	MixinID      string               `json:"mixin_id" binding:"required,min=1"`
-	MixinName    string               `json:"mixin_name"`
-	OauthID      string               `json:"oauth_id" binding:"required,min=4"`
-	Credential   uiammodels.Attribute `json:"credential"`
+	Provider     uiammodels.AuthProviderTypeEnum `json:"provider"`
+	IdentityName string                          `json:"username"`
+	PhoneCode    string                          `json:"phone_code"`
+	PhoneNumber  string                          `json:"phone_number"`
+	Email        string                          `json:"email"`
+	AvatarURL    string                          `json:"avatar_url"`
+	Description  string                          `json:"description"`
+	OauthID      string                          `json:"oauth_id" binding:"required,min=4"`
+	UnionID      string                          `json:"union_id"`
+	AppUserID    string                          `json:"app_user_id"`
+	AppUserName  string                          `json:"app_user_name"`
+	Credential   uiammodels.Attribute            `json:"credential"`
 }
 
-// AuthWechatCreateRequest IdentityRequest
-type AuthWechatCreateRequest struct {
+// AuthCreateRequest AuthCreateRequest
+type AuthCreateRequest struct {
 	BaseRequest
 
-	IdentityName string               `json:"username"`
-	PhoneCode    string               `json:"phone_code"`
-	PhoneNumber  string               `json:"phone_number"`
-	Email        string               `json:"email"`
-	AvatarURL    string               `json:"avatar_url"`
-	Description  string               `json:"description"`
-	OauthID      string               `json:"oauth_id" binding:"required,min=4"`
-	UnionID      string               `json:"union_id"`
-	Credential   uiammodels.Attribute `json:"credential"`
+	Provider    uiammodels.AuthProviderTypeEnum `json:"provider"`
+	OauthID     string                          `json:"oauth_id" binding:"required,min=4"`
+	UnionID     string                          `json:"union_id"`
+	AppUserID   string                          `json:"app_user_id"`
+	AppUserName string                          `json:"app_user_name"`
+	Credential  uiammodels.Attribute            `json:"credential"`
 }
