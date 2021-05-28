e92ba17773543b659a9cc169c053f48744910d05
Zhiming Jiang <lanvige@gmail.com>
2021-05-28 12:11:42 +0800

    update id

--- responses/identity_response.go
+++ responses/identity_response.go
@@ -8,18 +8,26 @@ import (
 
 // Identity Identity
 type Identity struct {
-	IdentityID   uint64                       `json:"-"`
-	IdentityUUID string                       `json:"uuid"`
-	Username     string                       `json:"username"`
-	Nickname     string                       `json:"nickname"`
-	PhoneCode    string                       `json:"phone_code"`
-	PhoneNumber  string                       `json:"phone_number"`
-	Email        string                       `json:"email,omitempty"`
-	AvatarURL    string                       `json:"avatar_url"`
-	Description  string                       `json:"description"`
-	Auths        *Auths                       `json:"authorizations,omitempty"`
-	Profile      *Profile                     `json:"profile,omitempty"`
-	Attributes   uiammodel.Attribute          `json:"attributes,omitempty"`
-	Status       uiammodel.IdentityStatusEnum `json:"status"`
-	CreatedAt    time.Time                    `json:"created_at"`
+	IdentityID    uint64                       `json:"-"`
+	IdentityUUID  string                       `json:"uuid"`
+	Slug          string                       `json:"slug"`
+	Username      string                       `json:"username"`
+	Nickname      string                       `json:"nickname"`
+	PhoneCode     string                       `json:"phone_code"`
+	PhoneNumber   string                       `json:"phone_number"`
+	PhoneVerified bool                         `json:"phone_verified,omitempty"`
+	Email         string                       `json:"email,omitempty"`
+	EmailVerified bool                         `json:"email_verified,omitempty"`
+	AvatarURL     string                       `json:"avatar_url"`
+	Description   string                       `json:"description"`
+	MfaType       uiammodel.MfaTypeEnum        `json:"mfa_type,omitempty"`
+	MfaStatus     uiammodel.MfaStatusEnum      `json:"mfa_status,omitempty"`
+	MfaPayload    uiammodel.Attribute          `json:"mfa_payload,omitempty"`
+	Remark        string                       `json:"remark,omitempty"`
+	Auths         *Auths                       `json:"authorizations,omitempty"`
+	Profile       *Profile                     `json:"profile,omitempty"`
+	Attributes    uiammodel.Attribute          `json:"attributes,omitempty"`
+	Status        uiammodel.IdentityStatusEnum `json:"status"`
+	CreatedAt     time.Time                    `json:"created_at"`
+	UpdatedAt     time.Time                    `json:"updated_at"`
 }
--- responses/realm_response.go
+++ responses/realm_response.go
@@ -8,12 +8,23 @@ import (
 
 // Realm
 type Realm struct {
-	Slug        string                    `json:"slug"`
-	Name        string                    `json:"name"`
-	AvatarURL   string                    `json:"avatar_url"`
-	AuthModel   string                    `json:"auth_model"`
-	Description string                    `json:"description"`
-	Remark      string                    `json:"remark"`
-	Status      uiammodel.RealmStatusEnum `json:"status"`
-	CreatedAt   time.Time                 `json:"created_at"`
+	Slug            string                             `json:"id"`
+	Name            string                             `json:"name"`
+	Description     string                             `json:"description"`
+	AvatarURL       string                             `json:"avatar_url"`
+	AuthnModel      uiammodel.RealmAuthzModelEnum      `json:"authn_model"`
+	CaptchaMode     uiammodel.RealmCaptchaModeEnum     `json:"captcha_model"`
+	CaptchaProvider uiammodel.RealmCaptchaProviderEnum `json:"captcha_provider"`
+	CaptchaPayload  uiammodel.Attribute                `json:"captcha_payload"`
+	AuthzModel      uiammodel.RealmAuthzModelEnum      `json:"authz_model"`
+	PasswordRule    uiammodel.RealmAuthzModelEnum      `json:"password_rule"`
+	MfaRule         string                             `json:"mfa_rule"`
+	LoginRule       string                             `json:"login_rule"`
+	Secret          string                             `json:"secret"`
+	SecretCreateAt  *time.Time                         `json:"secret_at"`
+	RequestURLs     string                             `json:"request_urls"`
+	Attributes      uiammodel.Attribute                `json:"attributes,omitempty"`
+	Remark          string                             `json:"remark"`
+	Status          uiammodel.RealmStatusEnum          `json:"status"`
+	CreatedAt       time.Time                          `json:"created_at"`
 }
